8bbfae855d589a8ce14dc13c2a81b5fd0da7dff5
Renan Lazarini Gil <renan@freework.io>
2018-05-04 11:52:35 +0200

    minified

--- index-ecma6.js
+++ index-ecma6.js
@@ -1,18 +1,18 @@
-'use strict';
+'use strict'
 
-const assert = require('assert');
-const removeSlash = require('remove-trailing-slash');
-const looselyValidate = require('@segment/loosely-validate-event');
-const axios = require('axios');
-const axiosRetry = require('axios-retry');
-const ms = require('ms');
-const uuid = require('uuid/v4');
-const md5 = require('md5');
-const version = require('./package.json').version;
-const isString = require('lodash.isstring');
+const assert = require('assert')
+const removeSlash = require('remove-trailing-slash')
+const looselyValidate = require('@segment/loosely-validate-event')
+const axios = require('axios')
+const axiosRetry = require('axios-retry')
+const ms = require('ms')
+const uuid = require('uuid/v4')
+const md5 = require('md5')
+const version = require('./package.json').version
+const isString = require('lodash.isstring')
 
-const setImmediate = global.setImmediate || process.nextTick.bind(process);
-const noop = () => {};
+const setImmediate = global.setImmediate || process.nextTick.bind(process)
+const noop = () => {}
 
 class Analytics {
   /**
@@ -27,41 +27,41 @@ class Analytics {
    *   @property {Boolean} enable (default: true)
    */
 
-  constructor(writeKey, options) {
-    options = options || {};
+  constructor (writeKey, options) {
+    options = options || {}
 
-    assert(writeKey, 'You must pass your Segment project\'s write key.');
+    assert(writeKey, 'You must pass your Segment project\'s write key.')
 
-    this.queue = [];
-    this.writeKey = writeKey;
-    this.host = removeSlash(options.host || 'https://api.segment.io');
-    this.timeout = options.timeout || false;
-    this.flushAt = Math.max(options.flushAt, 1) || 20;
-    this.flushInterval = options.flushInterval || 10000;
-    this.flushed = false;
+    this.queue = []
+    this.writeKey = writeKey
+    this.host = removeSlash(options.host || 'https://api.segment.io')
+    this.timeout = options.timeout || false
+    this.flushAt = Math.max(options.flushAt, 1) || 20
+    this.flushInterval = options.flushInterval || 10000
+    this.flushed = false
     Object.defineProperty(this, 'enable', {
       configurable: false,
       writable: false,
       enumerable: true,
       value: typeof options.enable === 'boolean' ? options.enable : true
-    });
+    })
 
     axiosRetry(axios, {
       retries: options.retryCount || 3,
       retryCondition: this._isErrorRetryable,
       retryDelay: axiosRetry.exponentialDelay
-    });
+    })
   }
 
-  _validate(message, type) {
+  _validate (message, type) {
     try {
-      looselyValidate(message, type);
+      looselyValidate(message, type)
     } catch (e) {
       if (e.message === 'Your message must be < 32kb.') {
-        console.log('Your message must be < 32kb. This is currently surfaced as a warning to allow clients to update. Versions released after August 1, 2018 will throw an error instead. Please update your code before then.', message);
-        return;
+        console.log('Your message must be < 32kb. This is currently surfaced as a warning to allow clients to update. Versions released after August 1, 2018 will throw an error instead. Please update your code before then.', message)
+        return
       }
-      throw e;
+      throw e
     }
   }
 
@@ -73,10 +73,10 @@ class Analytics {
    * @return {Analytics}
    */
 
-  identify(message, callback) {
-    this._validate(message, 'identify');
-    this.enqueue('identify', message, callback);
-    return this;
+  identify (message, callback) {
+    this._validate(message, 'identify')
+    this.enqueue('identify', message, callback)
+    return this
   }
 
   /**
@@ -87,10 +87,10 @@ class Analytics {
    * @return {Analytics}
    */
 
-  group(message, callback) {
-    this._validate(message, 'group');
-    this.enqueue('group', message, callback);
-    return this;
+  group (message, callback) {
+    this._validate(message, 'group')
+    this.enqueue('group', message, callback)
+    return this
   }
 
   /**
@@ -101,10 +101,10 @@ class Analytics {
    * @return {Analytics}
    */
 
-  track(message, callback) {
-    this._validate(message, 'track');
-    this.enqueue('track', message, callback);
-    return this;
+  track (message, callback) {
+    this._validate(message, 'track')
+    this.enqueue('track', message, callback)
+    return this
   }
 
   /**
@@ -115,10 +115,10 @@ class Analytics {
    * @return {Analytics}
    */
 
-  page(message, callback) {
-    this._validate(message, 'page');
-    this.enqueue('page', message, callback);
-    return this;
+  page (message, callback) {
+    this._validate(message, 'page')
+    this.enqueue('page', message, callback)
+    return this
   }
 
   /**
@@ -129,10 +129,10 @@ class Analytics {
    * @return {Analytics}
    */
 
-  screen(message, callback) {
-    this._validate(message, 'screen');
-    this.enqueue('screen', message, callback);
-    return this;
+  screen (message, callback) {
+    this._validate(message, 'screen')
+    this.enqueue('screen', message, callback)
+    return this
   }
 
   /**
@@ -143,10 +143,10 @@ class Analytics {
    * @return {Analytics}
    */
 
-  alias(message, callback) {
-    this._validate(message, 'alias');
-    this.enqueue('alias', message, callback);
-    return this;
+  alias (message, callback) {
+    this._validate(message, 'alias')
+    this.enqueue('alias', message, callback)
+    return this
   }
 
   /**
@@ -159,28 +159,28 @@ class Analytics {
    * @api private
    */
 
-  enqueue(type, message, callback) {
-    callback = callback || noop;
+  enqueue (type, message, callback) {
+    callback = callback || noop
 
     if (!this.enable) {
-      return setImmediate(callback);
+      return setImmediate(callback)
     }
 
-    message = Object.assign({}, message);
-    message.type = type;
+    message = Object.assign({}, message)
+    message.type = type
     message.context = Object.assign({
       library: {
         name: 'analytics-node',
         version
       }
-    }, message.context);
+    }, message.context)
 
     message._metadata = Object.assign({
       nodeVersion: process.versions.node
-    }, message._metadata);
+    }, message._metadata)
 
     if (!message.timestamp) {
-      message.timestamp = new Date();
+      message.timestamp = new Date()
     }
 
     if (!message.messageId) {
@@ -188,33 +188,33 @@ class Analytics {
       // for use in the browser where the uuid package falls back to Math.random()
       // which is not a great source of randomness.
       // Borrowed from analytics.js (https://github.com/segment-integrations/analytics.js-integration-segmentio/blob/a20d2a2d222aeb3ab2a8c7e72280f1df2618440e/lib/index.js#L255-L256).
-      message.messageId = `node-${md5(JSON.stringify(message))}-${uuid()}`;
+      message.messageId = `node-${md5(JSON.stringify(message))}-${uuid()}`
     }
 
     // Historically this library has accepted strings and numbers as IDs.
     // However, our spec only allows strings. To avoid breaking compatibility,
     // we'll coerce these to strings if they aren't already.
     if (message.anonymousId && !isString(message.anonymousId)) {
-      message.anonymousId = JSON.stringify(message.anonymousId);
+      message.anonymousId = JSON.stringify(message.anonymousId)
     }
     if (message.userId && !isString(message.userId)) {
-      message.userId = JSON.stringify(message.userId);
+      message.userId = JSON.stringify(message.userId)
     }
 
-    this.queue.push({ message, callback });
+    this.queue.push({ message, callback })
 
     if (!this.flushed) {
-      this.flushed = true;
-      this.flush();
-      return;
+      this.flushed = true
+      this.flush()
+      return
     }
 
     if (this.queue.length >= this.flushAt) {
-      this.flush();
+      this.flush()
     }
 
     if (this.flushInterval && !this.timer) {
-      this.timer = setTimeout(this.flush.bind(this), this.flushInterval);
+      this.timer = setTimeout(this.flush.bind(this), this.flushInterval)
     }
   }
 
@@ -225,44 +225,44 @@ class Analytics {
    * @return {Analytics}
    */
 
-  flush(callback) {
-    callback = callback || noop;
+  flush (callback) {
+    callback = callback || noop
 
     if (!this.enable) {
-      return setImmediate(callback);
+      return setImmediate(callback)
     }
 
     if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = null;
+      clearTimeout(this.timer)
+      this.timer = null
     }
 
     if (!this.queue.length) {
-      return setImmediate(callback);
+      return setImmediate(callback)
     }
 
-    const items = this.queue.splice(0, this.flushAt);
-    const callbacks = items.map(item => item.callback);
-    const messages = items.map(item => item.message);
+    const items = this.queue.splice(0, this.flushAt)
+    const callbacks = items.map(item => item.callback)
+    const messages = items.map(item => item.message)
 
     const data = {
       batch: messages,
       timestamp: new Date(),
       sentAt: new Date()
-    };
+    }
 
     const done = err => {
-      callbacks.forEach(callback => callback(err));
-      callback(err, data);
-    };
+      callbacks.forEach(callback => callback(err))
+      callback(err, data)
+    }
 
     // Don't set the user agent if we're not on a browser. The latest spec allows
     // the User-Agent header (see https://fetch.spec.whatwg.org/#terminology-headers
     // and https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/setRequestHeader),
     // but browsers such as Chrome and Safari have not caught up.
-    const headers = {};
+    const headers = {}
     if (typeof window === 'undefined') {
-      headers['user-agent'] = `analytics-node/${version}`;
+      headers['user-agent'] = `analytics-node/${version}`
     }
 
     const req = {
@@ -273,45 +273,47 @@ class Analytics {
       },
       data,
       headers
-    };
-
-    if (this.timeout) {
-      req.timeout = typeof this.timeout === 'string' ? ms(this.timeout) : this.timeout;
     }
 
-    axios(req).then(() => done()).catch(err => {
-      if (err.response) {
-        const error = new Error(err.response.statusText);
-        return done(error);
-      }
+    if (this.timeout) {
+      req.timeout = typeof this.timeout === 'string' ? ms(this.timeout) : this.timeout
+    }
 
-      done(err);
-    });
+    axios(req)
+      .then(() => done())
+      .catch(err => {
+        if (err.response) {
+          const error = new Error(err.response.statusText)
+          return done(error)
+        }
+
+        done(err)
+      })
   }
 
-  _isErrorRetryable(error) {
+  _isErrorRetryable (error) {
     // Retry Network Errors.
     if (axiosRetry.isNetworkError(error)) {
-      return true;
+      return true
     }
 
     if (!error.response) {
       // Cannot determine if the request can be retried
-      return false;
+      return false
     }
 
     // Retry Server Errors (5xx).
     if (error.response.status >= 500 && error.response.status <= 599) {
-      return true;
+      return true
     }
 
     // Retry if rate limited.
     if (error.response.status === 429) {
-      return true;
+      return true
     }
 
-    return false;
+    return false
   }
 }
 
-module.exports = Analytics;
+module.exports = Analytics
